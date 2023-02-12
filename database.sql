5456b6d6c33956249e3e40a4a8f311de6f15048b
RuslanUC <dev_ruslan_uc@protonmail.com>
2023-02-12 17:23:40 +0200

    refactor: add foreign keys to tables

--- database.sql
+++ database.sql
@@ -1,11 +1,18 @@
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL PRIMARY KEY,
   `email` longtext NOT NULL,
   `password` longtext NOT NULL,
   `key` longtext NOT NULL,
-  `verified` bool NOT NULL DEFAULT false,
-  UNIQUE KEY `id` (`id`) USING HASH
+  `verified` bool NOT NULL DEFAULT false
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `uid` bigint NOT NULL,
+  `sid` bigint NOT NULL,
+  `sig` longtext NOT NULL,
+  FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `userdata`;
@@ -24,21 +31,8 @@ CREATE TABLE `userdata` (
   `bio` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT "",
   `flags` bigint DEFAULT 0,
   `public_flags` bigint DEFAULT 0,
-  UNIQUE KEY `uid` (`uid`) USING HASH
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `sessions`;
-CREATE TABLE `sessions` (
-  `uid` bigint NOT NULL,
-  `sid` bigint NOT NULL,
-  `sig` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `relationships`;
-CREATE TABLE `relationships` (
-  `u1` bigint NOT NULL,
-  `u2` bigint NOT NULL,
-  `type` int NOT NULL
+  UNIQUE KEY (`uid`),
+  FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `settings`;
@@ -91,148 +85,38 @@ CREATE TABLE `settings` (
   `expression_suggestions_enabled` bool NOT NULL DEFAULT true,
   `view_image_descriptions` bool NOT NULL DEFAULT true,
   `dismissed_contents` tinytext NOT NULL DEFAULT "510109000002000080",
-  UNIQUE KEY `uid` (`uid`) USING HASH
+  UNIQUE KEY `uid` (`uid`),
+  FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `frecency_settings`;
+CREATE TABLE `frecency_settings` (
+  `uid` bigint NOT NULL,
+  `settings` longtext NOT NULL,
+  UNIQUE KEY `uid` (`uid`),
+  FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `relationships`;
+CREATE TABLE `relationships` (
+  `u1` bigint NOT NULL,
+  `u2` bigint NOT NULL,
+  `type` int NOT NULL,
+  FOREIGN KEY (`u1`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`u2`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `mfa_codes`;
 CREATE TABLE `mfa_codes` (
   `uid` bigint NOT NULL,
   `code` text NOT NULL,
-  `used` bool NOT NULL DEFAULT false
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `channels`;
-CREATE TABLE `channels` (
-  `id` bigint NOT NULL,
-  `type` int NOT NULL,
-  `guild_id` bigint DEFAULT NULL,
-  `position` int DEFAULT NULL,
-  `j_permission_overwrites` JSON DEFAULT NULL,
-  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `topic` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `nsfw` bool DEFAULT NULL,
-  `bitrate` int DEFAULT NULL,
-  `user_limit` int DEFAULT NULL,
-  `rate_limit` int DEFAULT NULL,
-  `j_recipients` JSON DEFAULT NULL,
-  `icon` text DEFAULT NULL,
-  `owner_id` bigint DEFAULT NULL,
-  `application_id` bigint DEFAULT NULL,
-  `parent_id` bigint DEFAULT NULL,
-  `rtc_region` text DEFAULT NULL,
-  `video_quality_mode` int DEFAULT NULL,
-  `j_thread_metadata` JSON DEFAULT NULL,
-  `default_auto_archive` int DEFAULT NULL,
-  `flags` int DEFAULT NULL,
-  UNIQUE KEY `id` (`id`) USING HASH
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `messages`;
-CREATE TABLE `messages` (
-  `id` bigint NOT NULL,
-  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `channel_id` bigint NOT NULL,
-  `author` bigint NOT NULL,
-  `edit_timestamp` bigint DEFAULT NULL,
-  `j_embeds` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT "[]" CHECK(json_valid(`j_embeds`)),
-  `pinned` bool NOT NULL DEFAULT false,
-  `webhook_id` bigint DEFAULT NULL,
-  `application_id` bigint DEFAULT NULL,
-  `type` int DEFAULT 0,
-  `flags` int DEFAULT 0,
-  `message_reference` bigint DEFAULT NULL,
-  `thread` bigint DEFAULT NULL,
-  `j_components` JSON NOT NULL DEFAULT "[]",
-  `j_sticker_items` JSON NOT NULL DEFAULT "[]",
-  `j_extra_data` JSON NOT NULL DEFAULT "{}",
-  `guild_id` bigint DEFAULT NULL,
-  UNIQUE KEY `id` (`id`) USING HASH
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `read_states`;
-CREATE TABLE `read_states` (
-  `uid` bigint NOT NULL,
-  `channel_id` bigint NOT NULL,
-  `last_read_id` bigint NOT NULL,
-  `count` int NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `notes`;
-CREATE TABLE `notes` (
-  `uid` bigint NOT NULL,
-  `target_uid` bigint NOT NULL,
-  `note` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `attachments`;
-CREATE TABLE `attachments` (
-  `id` bigint NOT NULL,
-  `channel_id` bigint NOT NULL,
-  `message_id` bigint NOT NULL,
-  `filename` text NOT NULL,
-  `size` bigint NOT NULL,
-  `uuid` text NOT NULL,
-  `content_type` text DEFAULT NULL,
-  `uploaded` bool NOT NULL DEFAULT false,
-  `j_metadata` JSON NOT NULL DEFAULT "{}",
-  UNIQUE KEY `id` (`id`) USING HASH
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-#DROP TABLE IF EXISTS `connections`;
-#CREATE TABLE `connections` (
-#  `uid` bigint NOT NULL,
-#  `type` text NOT NULL,
-#  `state` text DEFAULT NULL,
-#  `username` text DEFAULT NULL,
-#  `service_uid` bigint DEFAULT NULL,
-#  `friend_sync` bool NOT NULL DEFAULT false,
-#  `j_integrations` JSON NOT NULL DEFAULT "[]",
-#  `visible` bool NOT NULL DEFAULT true,
-#  `verified` bool NOT NULL DEFAULT true,
-#  `revoked` bool NOT NULL DEFAULT false,
-#  `show_activity` bool NOT NULL DEFAULT true,
-#  `two_way_link` bool NOT NULL DEFAULT false
-#) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `frecency_settings`;
-CREATE TABLE `frecency_settings` (
-  `uid` bigint NOT NULL,
-  `settings` longtext NOT NULL,
-  UNIQUE KEY `uid` (`uid`) USING HASH
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `guild_templates`;
-CREATE TABLE `guild_templates` (
-  `id` bigint NOT NULL,
-  `updated_at` bigint NOT NULL,
-  `template` JSON NOT NULL DEFAULT "{}",
-  UNIQUE KEY `id` (`id`) USING HASH
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `reactions`;
-CREATE TABLE `reactions` (
-  `message_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `emoji_id` bigint DEFAULT NULL,
-  `emoji_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-DROP TABLE IF EXISTS `invites`;
-CREATE TABLE `invites` (
-  `id` bigint NOT NULL,
-  `channel_id` bigint NOT NULL,
-  `inviter` bigint NOT NULL,
-  `created_at` bigint NOT NULL,
-  `max_age` bigint NOT NULL,
-  `max_uses` bigint NOT NULL DEFAULT 0,
-  `guild_id` bigint DEFAULT NULL,
-  `type` int NOT NULL DEFAULT 1,
-  UNIQUE KEY `id` (`id`) USING HASH
+  `used` bool NOT NULL DEFAULT false,
+  FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `guilds`;
 CREATE TABLE `guilds` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL PRIMARY KEY,
   `owner_id` bigint NOT NULL,
   `name` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `icon` text DEFAULT NULL,
@@ -255,13 +139,12 @@ CREATE TABLE `guilds` (
   `preferred_locale` text NOT NULL DEFAULT "en-US",
   `premium_progress_bar_enabled` bool NOT NULL DEFAULT false,
   `nsfw` bool NOT NULL DEFAULT false,
-  `nsfw_level` int NOT NULL DEFAULT 0,
-  UNIQUE KEY `id` (`id`) USING HASH
+  `nsfw_level` int NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL PRIMARY KEY,
   `guild_id` bigint NOT NULL,
   `name` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `permissions` bigint NOT NULL DEFAULT 1071698660929,
@@ -273,7 +156,7 @@ CREATE TABLE `roles` (
   `icon` text DEFAULT NULL,
   `unicode_emoji` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `flags` int NOT NULL DEFAULT 0,
-  UNIQUE KEY `id` (`id`) USING HASH
+  FOREIGN KEY (`guild_id`) REFERENCES `guilds`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `guild_members`;
@@ -287,18 +170,20 @@ CREATE TABLE `guild_members` (
   `nick` text DEFAULT NULL,
   `j_roles` JSON NOT NULL DEFAULT "[]",
   `mute` bool NOT NULL DEFAULT false,
-  `deaf` bool NOT NULL DEFAULT false
+  `deaf` bool NOT NULL DEFAULT false,
+  FOREIGN KEY (`guild_id`) REFERENCES `guilds`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `guild_members_roles`;
 CREATE TABLE `guild_members_roles` (
   `user_id` bigint NOT NULL,
-  `role_id` bigint NOT NULL
+  `role_id` bigint NOT NULL,
+  FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `emojis`;
 CREATE TABLE `emojis` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL PRIMARY KEY,
   `name` text NOT NULL,
   `user_id` bigint NOT NULL,
   `guild_id` bigint NOT NULL,
@@ -307,12 +192,139 @@ CREATE TABLE `emojis` (
   `managed` bool NOT NULL DEFAULT false,
   `animated` bool NOT NULL DEFAULT false,
   `available` bool NOT NULL DEFAULT true,
-  UNIQUE KEY `id` (`id`) USING HASH
+  FOREIGN KEY (`guild_id`) REFERENCES `guilds`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `guild_bans`;
 CREATE TABLE `guild_bans` (
   `user_id` bigint NOT NULL,
   `guild_id` bigint NOT NULL,
-  `reason` text DEFAULT NULL
+  `reason` text DEFAULT NULL,
+  FOREIGN KEY (`guild_id`) REFERENCES `guilds`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `channels`;
+CREATE TABLE `channels` (
+  `id` bigint NOT NULL PRIMARY KEY,
+  `type` int NOT NULL,
+  `guild_id` bigint DEFAULT NULL,
+  `position` int DEFAULT NULL,
+  `j_permission_overwrites` JSON DEFAULT NULL,
+  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `topic` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nsfw` bool DEFAULT NULL,
+  `bitrate` int DEFAULT NULL,
+  `user_limit` int DEFAULT NULL,
+  `rate_limit` int DEFAULT NULL,
+  `j_recipients` JSON DEFAULT NULL,
+  `icon` text DEFAULT NULL,
+  `owner_id` bigint DEFAULT NULL,
+  `application_id` bigint DEFAULT NULL,
+  `parent_id` bigint DEFAULT NULL,
+  `rtc_region` text DEFAULT NULL,
+  `video_quality_mode` int DEFAULT NULL,
+  `j_thread_metadata` JSON DEFAULT NULL,
+  `default_auto_archive` int DEFAULT NULL,
+  `flags` int DEFAULT NULL,
+  FOREIGN KEY (`guild_id`) REFERENCES `guilds`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE `messages` (
+  `id` bigint NOT NULL PRIMARY KEY,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `channel_id` bigint NOT NULL,
+  `author` bigint NOT NULL,
+  `edit_timestamp` bigint DEFAULT NULL,
+  `j_embeds` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT "[]" CHECK(json_valid(`j_embeds`)),
+  `pinned` bool NOT NULL DEFAULT false,
+  `webhook_id` bigint DEFAULT NULL,
+  `application_id` bigint DEFAULT NULL,
+  `type` int DEFAULT 0,
+  `flags` int DEFAULT 0,
+  `message_reference` bigint DEFAULT NULL,
+  `thread` bigint DEFAULT NULL,
+  `j_components` JSON NOT NULL DEFAULT "[]",
+  `j_sticker_items` JSON NOT NULL DEFAULT "[]",
+  `j_extra_data` JSON NOT NULL DEFAULT "{}",
+  `guild_id` bigint DEFAULT NULL,
+  FOREIGN KEY (`channel_id`) REFERENCES `channels`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `attachments`;
+CREATE TABLE `attachments` (
+  `id` bigint NOT NULL PRIMARY KEY,
+  `channel_id` bigint NOT NULL,
+  `message_id` bigint NOT NULL,
+  `filename` text NOT NULL,
+  `size` bigint NOT NULL,
+  `uuid` text NOT NULL,
+  `content_type` text DEFAULT NULL,
+  `uploaded` bool NOT NULL DEFAULT false,
+  `j_metadata` JSON NOT NULL DEFAULT "{}",
+  FOREIGN KEY (`message_id`) REFERENCES `messages`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `reactions`;
+CREATE TABLE `reactions` (
+  `message_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `emoji_id` bigint DEFAULT NULL,
+  `emoji_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  FOREIGN KEY (`message_id`) REFERENCES `messages`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `invites`;
+CREATE TABLE `invites` (
+  `id` bigint NOT NULL PRIMARY KEY,
+  `channel_id` bigint NOT NULL,
+  `inviter` bigint NOT NULL,
+  `created_at` bigint NOT NULL,
+  `max_age` bigint NOT NULL,
+  `max_uses` bigint NOT NULL DEFAULT 0,
+  `guild_id` bigint DEFAULT NULL,
+  `type` int NOT NULL DEFAULT 1,
+  FOREIGN KEY (`channel_id`) REFERENCES `channels`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `read_states`;
+CREATE TABLE `read_states` (
+  `uid` bigint NOT NULL,
+  `channel_id` bigint NOT NULL,
+  `last_read_id` bigint NOT NULL,
+  `count` int NOT NULL DEFAULT 0,
+  FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`channel_id`) REFERENCES `channels`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `notes`;
+CREATE TABLE `notes` (
+  `uid` bigint NOT NULL,
+  `target_uid` bigint NOT NULL,
+  `note` text NOT NULL,
+  FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`target_uid`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+#DROP TABLE IF EXISTS `guild_templates`;
+#CREATE TABLE `guild_templates` (
+#  `id` bigint NOT NULL,
+#  `updated_at` bigint NOT NULL,
+#  `template` JSON NOT NULL DEFAULT "{}"
+#) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+#DROP TABLE IF EXISTS `connections`;
+#CREATE TABLE `connections` (
+#  `uid` bigint NOT NULL,
+#  `type` text NOT NULL,
+#  `state` text DEFAULT NULL,
+#  `username` text DEFAULT NULL,
+#  `service_uid` bigint DEFAULT NULL,
+#  `friend_sync` bool NOT NULL DEFAULT false,
+#  `j_integrations` JSON NOT NULL DEFAULT "[]",
+#  `visible` bool NOT NULL DEFAULT true,
+#  `verified` bool NOT NULL DEFAULT true,
+#  `revoked` bool NOT NULL DEFAULT false,
+#  `show_activity` bool NOT NULL DEFAULT true,
+#  `two_way_link` bool NOT NULL DEFAULT false
+#) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
