b7fcf5d16bee0f6ad063743cf8cf8774a2679942
RuslanUC <dev_ruslan_uc@protonmail.com>
2023-08-06 19:45:07 +0300

    add "tests" for hardcoded (other.py) endpoints

--- tests/test_http.py
+++ tests/test_http.py
@@ -1441,3 +1441,104 @@ async def test_change_username_dont_change_discriminator(testapp):
     json = await resp.get_json()
     assert json["username"] == data2["username"]
     assert json["discriminator"] != data2["discriminator"]
+
+
+@pt.mark.asyncio
+async def test_hardcoded_endpoints(testapp):
+    client: TestClientType = (await testapp).test_client()
+
+    resp = await client.get(f"/api/v9/auth/location-metadata")
+    assert resp.status_code == 200
+
+    resp = await client.post(f"/api/v9/science")
+    assert resp.status_code == 204
+
+    resp = await client.get(f"/api/v9/experiments")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/applications/detectable")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/survey")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/affinities/guilds")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/affinities/users")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/library")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/billing/payment-sources")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/billing/country-code")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/billing/localized-pricing-promo")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/billing/user-trial-offer")
+    assert resp.status_code == 404
+
+    resp = await client.get(f"/api/v9/users/@me/billing/subscriptions")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/billing/subscription-slots")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/guilds/premium/subscription-slots")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/outbound-promotions")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/applications/0/entitlements")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/store/published-listings/skus/978380684370378762/subscription-plans")
+    assert resp.status_code == 200
+    resp = await client.get(f"/api/v9/store/published-listings/skus/521846918637420545/subscription-plans")
+    assert resp.status_code == 200
+    resp = await client.get(f"/api/v9/store/published-listings/skus/521847234246082599/subscription-plans")
+    assert resp.status_code == 200
+    resp = await client.get(f"/api/v9/store/published-listings/skus/590663762298667008/subscription-plans")
+    assert resp.status_code == 200
+    resp = await client.get(f"/api/v9/store/published-listings/skus/1/subscription-plans")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/outbound-promotions/codes")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/entitlements/gifts")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/activities/statistics/applications")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/billing/payments")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/settings-proto/3")
+    assert resp.status_code == 200
+
+    resp = await client.patch(f"/api/v9/users/@me/settings-proto/3")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/users/@me/settings-proto/4")
+    assert resp.status_code == 400
+
+    resp = await client.patch(f"/api/v9/users/@me/settings-proto/4")
+    assert resp.status_code == 400
+
+    resp = await client.get(f"/api/v9/gateway")
+    assert resp.status_code == 200
+    assert await resp.get_json() == {"url": f"wss://{Config.GATEWAY_HOST}"}
+
+    resp = await client.get(f"/api/v9/oauth2/tokens")
+    assert resp.status_code == 200
+
+    resp = await client.get(f"/api/v9/sticker-packs")
+    assert resp.status_code == 200
