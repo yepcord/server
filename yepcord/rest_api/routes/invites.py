e72e7b341aad46c2f4f58c160aea6a92e5a1ff55
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-03-12 09:04:02 +0200

    update year in license headers

--- yepcord/rest_api/routes/invites.py
+++ yepcord/rest_api/routes/invites.py
@@ -1,6 +1,6 @@
 """
     YEPCord: Free open source selfhostable fully discord-compatible chat
-    Copyright (C) 2022-2023 RuslanUC
+    Copyright (C) 2022-2024 RuslanUC
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
