b6f83852dcb53a900690ea147e8b3c1f7231f7a3
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-03-15 18:34:23 +0200

    idk

--- yepcord/voice_gateway/gateway.py
+++ yepcord/voice_gateway/gateway.py
@@ -3,12 +3,15 @@ from __future__ import annotations
 from os import urandom
 from typing import Optional
 
+from httpx import AsyncClient
 from quart import Websocket
+from semanticsdp import SDPInfo
 
 from yepcord.yepcord.enums import VoiceGatewayOp
 from .default_sdp import DEFAULT_SDP
 from .events import Event, ReadyEvent, SpeakingEvent, UdpSessionDescriptionEvent, RtcSessionDescriptionEvent
 from .schemas import SelectProtocol
+from .utils import convert_rtp_properties
 from ..gateway.utils import require_auth
 
 
@@ -24,6 +27,7 @@ class GatewayClient:
         self.rtx_ssrc = 0
         self.mode: Optional[str] = None
         self.key: Optional[bytes] = None
+        self.sdp = SDPInfo.from_dict(DEFAULT_SDP)
 
         self._gw = gw
 
@@ -50,11 +54,13 @@ class GatewayClient:
         self.rtx_ssrc = self._gw.ssrc
         self._gw.ssrc += 1
 
-        await self.esend(ReadyEvent(self.ssrc, self.video_ssrc, self.rtx_ssrc, 0))
+        await self.esend(
+            ReadyEvent(self.ssrc, self.video_ssrc, self.rtx_ssrc, await self._gw.get_channel_port(self.guild_id))
+        )
 
     @require_auth(4003)
     async def handle_HEARTBEAT(self, data: dict):
-        await self.send({"op": VoiceGatewayOp.HEARTBEAT_ACK, "d": data["d"]})
+        await self.send({"op": VoiceGatewayOp.HEARTBEAT_ACK, "d": data})
 
     @require_auth(4003)
     async def handle_SELECT_PROTOCOL(self, data: dict):
@@ -65,16 +71,42 @@ class GatewayClient:
             return await self.ws.close(4012)
 
         if d.protocol == "webrtc":
+            offer = SDPInfo.parse(f"m=audio\n{d.sdp}")
+            self.sdp.ice = offer.ice
+            self.sdp.dtls = offer.dtls
+            
+            port = await self._gw.get_channel_port(self.guild_id)
+            _, fingerprint = await self._gw.get_media_server_info()
+
+            data = {
+                "local": {"ice": {"ufrag": urandom(8).hex(), "pwd": urandom(16).hex()}},
+                "remote": {
+                    "ice": {"ufrag": self.sdp.ice.ufrag, "pwd": self.sdp.ice.pwd},
+                    "dtls": {
+                        "hash": self.sdp.dtls.hash,
+                        "fingerprint": self.sdp.dtls.fingerprint,
+                        "setup": self.sdp.dtls.setup.value
+                    },
+                },
+                "props": convert_rtp_properties(self.sdp),
+                "strpProtectionProfiles": "",
+                "disableSTUNKeepAlive": False,
+                "disableREMB": False,
+            }
+
+            async with AsyncClient() as cl:
+                resp = await cl.post(f"http://127.0.0.1:9999/v1/channels/{self.guild_id}/users/{self.user_id}",
+                                     json=data)
 
             answer = (
-                    f"m=audio {...} ICE/SDP\n" +
-                    f"a=fingerprint:{...}\n" +
+                    f"m=audio {port} ICE/SDP\n" +
+                    f"a=fingerprint:sha-256 {fingerprint}\n" +
                     f"c=IN IP4 127.0.0.1\n" +
-                    f"a=rtcp:{...}\n" +
-                    f"a=ice-ufrag:{...}\n" +
-                    f"a=ice-pwd:{...}\n" +
-                    f"a=fingerprint:{...}\n" +
-                    f"a=candidate:1 1 {...} {...} {...} {...} typ host\n"
+                    f"a=rtcp:{port}\n" +
+                    f"a=ice-ufrag:{data['local']['ice']['ufrag']}\n" +
+                    f"a=ice-pwd:{data['local']['ice']['pwd']}\n" +
+                    f"a=fingerprint:sha-256 {fingerprint}\n" +
+                    f"a=candidate:1 1 UDP 2130706431 127.0.0.1 {port} typ host\n"
             )
 
             await self.esend(RtcSessionDescriptionEvent(answer))
@@ -99,6 +131,30 @@ class GatewayClient:
 class Gateway:
     def __init__(self):
         self.ssrc = 1
+        self.address: Optional[str] = None
+        self.fingerprint: Optional[str] = None
+
+        self.channels = {}
+
+    async def get_media_server_info(self) -> tuple[str, str]:
+        if self.address is None or self.fingerprint is None:
+            async with AsyncClient() as cl:
+                resp = await cl.get("http://127.0.0.1:9999/v1")
+                j = resp.json()
+                return j["address"], j["fingerprint"]
+                self.address = j["address"]
+                self.fingerprint = j["fingerprint"]
+
+        return self.address, self.fingerprint
+
+    async def get_channel_port(self, channel_id: int) -> int:
+        if channel_id not in self.channels:
+            async with AsyncClient() as cl:
+                resp = await cl.post(f"http://127.0.0.1:9999/v1/channels/{channel_id}")
+                return resp.json()["port"]
+                self.channels[channel_id] = resp.json()["port"]
+
+        return self.channels[channel_id]
 
     async def sendHello(self, ws: Websocket) -> None:
         client = GatewayClient(ws, self)
